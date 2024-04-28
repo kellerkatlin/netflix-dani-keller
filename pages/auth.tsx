c548364816ab98ed185d20f777a78152d54efd39
kellerkatlin <kellerkatlin.k@gmail.com>
2024-04-28 01:34:27 -0500

    otro commit

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -96,21 +96,21 @@ export default function Auth() {
                         </button>
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                             <div
-                                // onClick={() =>
-                                //     signIn("google", {
-                                //         callbackUrl: "/profiles",
-                                //     })
-                                // }
+                                onClick={() =>
+                                    signIn("google", {
+                                        callbackUrl: "/profiles",
+                                    })
+                                }
                                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
                             >
                                 <FcGoogle size={30} />
                             </div>
                             <div
-                                // onClick={() =>
-                                //     signIn("github", {
-                                //         callbackUrl: "/profiles",
-                                //     })
-                                // }
+                                onClick={() =>
+                                    signIn("github", {
+                                        callbackUrl: "/profiles",
+                                    })
+                                }
                                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
                             >
                                 <FaGithub size={30} />
