3d6071c1af335d1437dbde48a09ee50abe0baf52
kellerkatlin <kellerkatlin.k@gmail.com>
2024-04-28 11:16:19 -0500

    fixsss

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,27 +1,22 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest } from "next";
 import { getSession } from "next-auth/react";
 import prismadb from "@/lib/prismadb";
 
-const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        const session = await getSession({ req });
+const serverAuth = async (req: NextApiRequest) => {
+    const session = await getSession({ req });
 
-        if (!session?.user?.email) {
-            return res.status(401).json({ error: "Not signed in" });
-        }
-        const currentUser = await prismadb.user.findUnique({
-            where: {
-                email: session.user.email,
-            },
-        });
-        if (!currentUser) {
-            return res.status(401).json({ error: "User not found" });
-        }
-        return res.status(200).json({ currentUser });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal server error" });
+    if (!session?.user?.email) {
+        throw new Error("Not signed in");
     }
+    const currentUser = await prismadb.user.findUnique({
+        where: {
+            email: session.user.email,
+        },
+    });
+    if (!currentUser) {
+        throw new Error("Not signed in");
+    }
+    return { currentUser };
 };
 
 export default serverAuth;
