6c27624d80554bb0023dfebb15d02de355db3d9d
kellerkatlin <kellerkatlin.k@gmail.com>
2024-04-28 11:39:24 -0500

    fixsss

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,9 +1,12 @@
-import swr from "swr";
 import fetcher from "@/lib/fetcher";
 import useSWR from "swr";
 
 const useCurrentUser = () => {
-    const { data, error, isLoading, mutate } = useSWR("/api/current", fetcher);
+    const { data, error, isLoading, mutate } = useSWR("/api/current", fetcher, {
+        revalidateOnFocus: false,
+        revalidateIfStale: false,
+        revalidateOnReconnect: false,
+    });
     return {
         data,
         error,
